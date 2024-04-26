54f5d094fb008ffa6b20d238882402607c4abb0f
Anne Do <doanneda@gmail.com>
2024-04-25 22:24:39 -0700

    Initial Commit

---
+++ kwkGithubTutorial/kwkGithubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwkGithubTutorialApp.swift
+//  kwkGithubTutorial
+//
+//  Created by Scholar on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct kwkGithubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
